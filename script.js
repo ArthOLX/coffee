6aa387877431c740251a0e5ab5699ad0f2c59ced
ArthOLX <tutuvaz1980@gmail.com>
2024-11-28 20:45:30 -0300

    animação

---
+++ script.js
@@ -0,0 +1,27 @@
+let animation = gsap.timeline();
+
+animation.to(".menu", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+})
+
+animation.to(".titulo-principal", {
+    x: 0,
+    opacity: 1,
+    duration: 0.4,
+})
+
+animation.to(".principal p", {
+    x: 0,
+    opacity: 1,
+    duration: 0.5,
+
+})
+
+animation.to(".principal button", {
+    x: 0,
+    opacity: 1,
+    duration: 0.1,
+    delay: 0
+})
